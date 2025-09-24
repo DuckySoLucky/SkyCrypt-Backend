7b243f4bd9a4ccdfb80dd8b9f9bf3f290579fd6d
DuckySoLucky <robertkovac160@gmail.com>
2025-09-24 16:25:14 +0200

    refactor: enable embed

--- src/routes/embed.go
+++ src/routes/embed.go
@@ -1,7 +1,11 @@
 package routes
 
 import (
+	"encoding/json"
 	"fmt"
+	"skycrypt/src/constants"
+	redis "skycrypt/src/db"
+	"skycrypt/src/models"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -12,7 +16,7 @@ func EmbedHandler(c *fiber.Ctx) error {
 
 	uuid := c.Params("uuid")
 	profileId := c.Params("profileId")
-	/*embed, err := redis.Get(fmt.Sprintf("embed:%s:%s", uuid, profileId))
+	embed, err := redis.Get(fmt.Sprintf("embed:%s:%s", uuid, profileId))
 	if err != nil {
 		c.Status(400)
 		return c.JSON(constants.InvalidUserError)
@@ -22,12 +26,11 @@ func EmbedHandler(c *fiber.Ctx) error {
 	if err := json.Unmarshal([]byte(embed), &embedData); err != nil {
 		c.Status(500)
 		return c.JSON(constants.InternalServerError)
-	}*/
+	}
 
 	fmt.Printf("Returning /api/embed/%s in %s\n", profileId, time.Since(timeNow))
 
-
 	return c.JSON(fiber.Map{
-		"embed": uuid,
+		"embed": embedData,
 	})
 }
